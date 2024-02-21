3b7aa6c119675849cf130d55ca2ee7c61abb42ea
akky25 <ry.akita1991@gmail.com>
2024-02-21 22:58:29 +0900

    side bar の作成

--- src/Components/Components.tsx
+++ src/Components/Components.tsx
@@ -1,2 +1,4 @@
 export { default as Navbar } from "./Navbar";
+export { default as Sidebar } from "./Sidebar";
+export { default as Laytout } from "./Layout";
 export { UserImage } from "./VideoComponent";
---
+++ src/Components/Layout.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Menu } from "./Icons/Icons";
+import Navbar from "./Navbar";
+import { Sidebar } from "./Components";
+
+interface LayoutProps {
+  children: JSX.Element;
+  closeSidebar?: boolean;
+}
+
+export default function Layout({ children, closeSidebar }: LayoutProps) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(" ");
+  }
+
+  return (
+    <>
+      <Navbar>
+        <button
+          type="button"
+          className="-mx-2 inline-flex items-center justify-center rounded-md p-2 focus:outline-none"
+          onClick={() => setSidebarOpen(true)}
+        >
+          <Menu className="h-6 w-6 stroke-gray-400" aria-hidden="true" />
+        </button>
+      </Navbar>
+      <Sidebar
+        isOpen={sidebarOpen}
+        closeSidebar={closeSidebar}
+        setSidebarOpen={setSidebarOpen}
+      />
+      <div className={classNames(closeSidebar ? "lg:pl-20" : "lg:pl-56")}>
+        <main className="py-24">
+          <div className="mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="space-x-4">{children}</div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
